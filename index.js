c541ff2c006615c5a0c9f6c4b1759d708f0c0e6d
James Barrington <james.barrington1985@gmail.com>
2020-03-20 21:34:44 +0000

    Procfile & Index changes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const app = require('./src/app');
 
-mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, err => {
-  console.log(err);
-  app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DATABASE_CONN, { useNewUrlParser: true }, () => {
+  app.listen(PORT, () => {
     // eslint-disable-next-line no-console
-    console.log('App listening on port 3000');
+    console.log('App listening on port', PORT);
   });
 });
